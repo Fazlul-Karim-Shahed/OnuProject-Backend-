0585a2067472019ad0ab2a0ec37e8b8698bfee4d
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2022-09-17 01:35:35 +0600

    Added properties add and remove button

--- Routers/RouterFuntions/CatalogFunctions/deleteCatalog.js
+++ Routers/RouterFuntions/CatalogFunctions/deleteCatalog.js
@@ -2,15 +2,19 @@
 const { Catalog } = require("../../../Models/CatalogModel")
 const _ = require('lodash')
 const { Categories } = require("../../../Models/CategoryModel")
+const { SubCategories } = require("../../../Models/SubCategoryModel")
+const { Products } = require("../../../Models/ProductModel")
 
 
 const deleteCatalog = async (req, res) => {
 
     const catalog = await Catalog.deleteOne({ _id: req.params.id })
     const category = await Categories.deleteMany({ catalogId: req.params.id })
-    console.log(catalog);
+    const subCategory = await SubCategories.deleteMany({ catalogId: req.params.id })
+    const products = await Products.deleteMany({ catalogId: req.params.id })
 
-    res.send({ message: 'Deleted catalogs', error: false, value: [catalog, category] })
+
+    res.send({ message: 'Deleted catalogs', error: false, value: [catalog, category, subCategory, products] })
 
 }
 
