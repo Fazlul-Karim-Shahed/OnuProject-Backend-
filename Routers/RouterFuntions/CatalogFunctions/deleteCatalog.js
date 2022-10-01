967e8182dcb77fb5ca937738a6f0a6bedb5806c2
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2022-10-02 01:20:39 +0600

    bug fix

--- Routers/RouterFuntions/CatalogFunctions/deleteCatalog.js
+++ Routers/RouterFuntions/CatalogFunctions/deleteCatalog.js
@@ -4,16 +4,17 @@ const _ = require('lodash')
 const { Categories } = require("../../../Models/CategoryModel")
 const { SubCategories } = require("../../../Models/SubCategoryModel")
 const { Products } = require("../../../Models/ProductModel")
-const { ProductProperties } = require("../../../Models/ProductPropertiesModel")
 
 
 const deleteCatalog = async (req, res) => {
 
-    const catalog = await Catalog.deleteOne({ _id: req.params.id })
-    const category = await Categories.deleteMany({ catalogId: req.params.id })
-    const subCategory = await SubCategories.deleteMany({ catalogId: req.params.id })
-    const products = await Products.deleteMany({ catalogId: req.params.id })
+    const id  = req.params.id
+    const catalog = await Catalog.deleteOne({ _id: id })
+    const category = await Categories.deleteMany({ catalogId: id })
+    const subCategory = await SubCategories.deleteMany({ catalogId: id })
+    const products = await Products.deleteMany({ catalogId: id })
 
+    console.log([catalog, category, subCategory, products]);
 
     res.send({ message: 'Deleted catalogs', error: false, value: [catalog, category, subCategory, products] })
 
